09c79d9341a89e3ffecbb10dc5e7560e2364d45b
mariia paraskun <mariiaparaskun@gmail.com>
2023-11-30 19:01:34 +0100

    ex01 c-style example added

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,11 +5,14 @@ int main() {
     uintptr_t result;
 
     foo.num = 5;
+
+    //uintptr_t cStyle = (uintptr_t)&foo;
     result = Serializer::serialize(&foo);
 
     std::cout << "data address: " << &foo << " num: " << foo.num << std::endl;
 
     std::cout << "Reinterpreted data: " << result << std::endl;
+    //std::cout << "cStyle: " << cStyle << std::endl;
 
     Data *deserialized = Serializer::deserialize(result);
     std::cout << "deserialized data address: " << deserialized << " num: " << deserialized->num << std::endl;
